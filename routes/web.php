9403de8b19f71797028cb0383dd9eaced6ef4075
peshraw <90914925+peshr4w@users.noreply.github.com>
2023-09-23 16:58:32 +0300

    d

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\GoogleController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('{any}', function () {
     return view('welcome');
-});
+})->where('any', ".*");
 
 
